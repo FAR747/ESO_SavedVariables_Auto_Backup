e0d909bab8d917979e1f216ebe90f5a3dcb943e3
FAR747 <far@hightgames.ru>
2020-08-07 17:42:36 +0300

    Добавьте файлы проекта.

---
+++ ESO SavedVariables Auto backup/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ESO_SavedVariables_Auto_backup
+{
+	/// <summary>
+	/// Логика взаимодействия для App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
